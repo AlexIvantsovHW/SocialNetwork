d3f35668ae09f615d2790c62f9657dd89b9d4314
unknown <alexander.ivantsov.hw@gmail.com>
2023-02-27 07:32:24 +0300

    upload users page. AJAX functions and Class Users included in UsersContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,67 +1,52 @@
 import React from "react";
 import s from "./Users.module.css";
-import axios from "axios";
 
-let anonimPhoto='https://krirpo.ru/wp-content/uploads/2021/11/noname.png';
 
-class Users extends React.Component {
-  
-  componentDidMount() {
-    debugger;
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-      .then((response) => {
-        this.props.SetUsers(response.data.items);
-      });
+const Users =(props)=> {
+  let anonimPhoto = "https://krirpo.ru/wp-content/uploads/2021/11/noname.png";
+  let pagesCount = Math.ceil(props.totalUsers / props.pageSize);
+  let dispayPages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    dispayPages.push(i);
   }
-  
-  onPostChange=(p)=>{
-    this.props.activePage(p)
-    axios
-    .get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count=${this.props.pageSize}`)
-    .then((response) => {
-      this.props.SetUsers(response.data.items);})
-  }
-  render() {
-    let pagesCount=Math.ceil(this.props.totalUsers/this.props.pageSize);
-    let dispayPages=[];
-    for (let i=1;i<=pagesCount;i++){dispayPages.push(i)};
 
-
-    let userElement = this.props.UserPage.users.map((el) => {
-      return (
-        <div>
-          <div className={s.container}>
-            <div>
-              <img src={el.photos.small===null?anonimPhoto:el.photos.small} className={s.ava} />
-              <div className={s.name}>{el.name}</div>
-              {el.followed ? (
-                <button
-                  className={s.button}
-                  onClick={() => {
-                    this.props.Follow(el.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  className={s.button}
-                  onClick={() => {
-                    this.props.Unfollow(el.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-            <div>
-              <div className={s.status}>{el.status}</div>
-            </div>
+  let userElement = props.UserPage.users.map((el) => {
+    return (
+      <div>
+        <div className={s.container}>
+          <div>
+            <img
+              src={el.photos.small === null ? anonimPhoto : el.photos.small}
+              className={s.ava}
+            />
+            <div className={s.name}>{el.name}</div>
+            {el.followed ? (
+              <button
+                className={s.button}
+                onClick={() => {
+                  props.Follow(el.id);
+                }}
+              >
+                Unfollow
+              </button>
+            ) : (
+              <button
+                className={s.button}
+                onClick={() => {
+                  props.Unfollow(el.id);
+                }}
+              >
+                Follow
+              </button>
+            )}
+          </div>
+          <div>
+            <div className={s.status}>{el.status}</div>
           </div>
         </div>
-      );
-    });
+      </div>
+    );
+  });
 
     return (
       <div>
@@ -71,13 +56,24 @@ class Users extends React.Component {
             <div>STATUS</div>
             <div>LOCATION</div>
           </div>
-        
-          <div>{dispayPages.map((p)=>{return(<span  onClick={(e)=>this.onPostChange(p)} className={s.activePage}> {p} </span>)})}</div>
+
+          <div>
+            {dispayPages.map((p) => {
+              return (
+                <span
+                  onClick={(e) =>props.onPostChange(p)}
+                  className={s.activePage}
+                >
+                  {" "}
+                  {p}{" "}
+                </span>
+              );
+            })}
+          </div>
         </div>
         {userElement}
       </div>
     );
-  }
-}
 
+}
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,41 @@
 import Users from "./Users";
 import { connect } from 'react-redux';
 import { followAC,unfollowAC,setUsersAC,activePageAC } from './../../redux/users-reducer';
+import axios from "axios";
+import { React } from 'react';
 
 
+class UsersAPIcomponent extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.SetUsers(response.data.items);
+      });
+  }
+
+  onPostChange = (p) => {
+    this.props.activePage(p);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${p}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.SetUsers(response.data.items);
+      });
+  };
+  render() {
+    return (
+      <Users
+        totalUsers={this.props.totalUsers}
+        pageSize={this.props.pageSize}
+        UserPage={this.props.UserPage}
+        onPostChange={this.onPostChange}/>
+    );
+  }
+}
 
 let mapToStateProps = (state) => {
   return { 
@@ -21,6 +54,6 @@ let mapToDispatch = (dispatch) => {
   };
 };
 
-const UsersContainer = connect(mapToStateProps, mapToDispatch)(Users);
+const UsersContainer = connect(mapToStateProps, mapToDispatch)(UsersAPIcomponent);
 
 export default UsersContainer;
